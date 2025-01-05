4361391405affbdbf6f693934531ea884ece06ab
Patrick Roza <contact@patrickroza.com>
2025-01-05 14:05:06 +0100

    m

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,8 +1,1 @@
 export * from "./lib.js"
-
-export * from "./makeClient.js"
-export * from "./makeContext.js"
-export * from "./makeIntl.js"
-export * from "./mutate.js"
-export * from "./query.js"
-export * from "./runtime.js"

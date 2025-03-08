9d63cae64a8c85df8b9f739abb814f9d8c231709
Patrick Roza <contact@patrickroza.com>
2025-03-08 11:01:47 +0100

    improve rpc embed

---
+++ packages/effect-app/src/canary/Rpc.ts
@@ -0,0 +1,49 @@
+/**
+ * @since 1.0.0
+ */
+export * as Rpc from "./Rpc/Rpc.js"
+
+/**
+ * @since 1.0.0
+ */
+export * as RpcClient from "./Rpc/RpcClient.js"
+
+/**
+ * @since 1.0.0
+ */
+export * as RpcGroup from "./Rpc/RpcGroup.js"
+
+/**
+ * @since 1.0.0
+ */
+export * as RpcMessage from "./Rpc/RpcMessage.js"
+
+/**
+ * @since 1.0.0
+ */
+export * as RpcMiddleware from "./Rpc/RpcMiddleware.js"
+
+/**
+ * @since 1.0.0
+ */
+export * as RpcSchema from "./Rpc/RpcSchema.js"
+
+/**
+ * @since 1.0.0
+ */
+export * as RpcSerialization from "./Rpc/RpcSerialization.js"
+
+/**
+ * @since 1.0.0
+ */
+export * as RpcServer from "./Rpc/RpcServer.js"
+
+/**
+ * @since 1.0.0
+ */
+export * as RpcTest from "./Rpc/RpcTest.js"
+
+/**
+ * @since 1.0.0
+ */
+export * as RpcWorker from "./Rpc/RpcWorker.js"

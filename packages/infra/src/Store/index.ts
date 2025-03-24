7b6ca2fa31b36d0c4eb8e77a4e5d4021e48a35f3
Patrick Roza <contact@patrickroza.com>
2025-03-24 18:21:00 +0100

    improve

--- packages/infra/src/Store/index.ts
+++ packages/infra/src/Store/index.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Redacted } from "effect-app"
+import { Effect, Layer, Redacted } from "effect-app"
 import { CosmosStoreLayer } from "./Cosmos.js"
 import { DiskStoreLayer } from "./Disk.js"
 import { MemoryStoreLive } from "./Memory.js"
@@ -7,23 +7,27 @@ import { MemoryStoreLive } from "./Memory.js"
 import type { StorageConfig } from "./service.js"
 
 export function StoreMakerLayer(cfg: StorageConfig) {
-  const storageUrl = Redacted.value(cfg.url)
-  if (storageUrl.startsWith("mem://")) {
-    console.log("Using in memory store")
-    return MemoryStoreLive
-  }
-  if (storageUrl.startsWith("disk://")) {
-    const dir = storageUrl.replace("disk://", "")
-    console.log("Using disk store at " + dir)
-    return DiskStoreLayer(cfg, dir)
-  }
-  // if (storageUrl.startsWith("redis://")) {
-  //   console.log("Using Redis store")
-  //   return RedisStoreLayer(cfg)
-  // }
+  return Effect
+    .sync(() => {
+      const storageUrl = Redacted.value(cfg.url)
+      if (storageUrl.startsWith("mem://")) {
+        console.log("Using in memory store")
+        return MemoryStoreLive
+      }
+      if (storageUrl.startsWith("disk://")) {
+        const dir = storageUrl.replace("disk://", "")
+        console.log("Using disk store at " + dir)
+        return DiskStoreLayer(cfg, dir)
+      }
+      // if (storageUrl.startsWith("redis://")) {
+      //   console.log("Using Redis store")
+      //   return RedisStoreLayer(cfg)
+      // }
 
-  console.log("Using Cosmos DB store")
-  return CosmosStoreLayer(cfg)
+      console.log("Using Cosmos DB store")
+      return CosmosStoreLayer(cfg)
+    })
+    .pipe(Layer.unwrapEffect)
 }
 
 export * from "./service.js"

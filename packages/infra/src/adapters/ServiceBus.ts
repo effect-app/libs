f864ff05bec2c6b8485d2416ab825b1a77639f16
Patrick Roza <contact@patrickroza.com>
2025-04-26 09:11:31 +0200

    add topic support to servicebus

--- packages/infra/src/adapters/ServiceBus.ts
+++ packages/infra/src/adapters/ServiceBus.ts
@@ -40,7 +40,7 @@ export function LiveSender(queueName: string) {
     .scoped(Sender, makeSender(queueName))
 }
 
-function makeReceiver(queueName: string, waitTillEmpty: Effect<void>, sessionId?: string) {
+function makeQueueReceiver(queueName: string, waitTillEmpty: Effect<void>, sessionId?: string) {
   return Effect.gen(function*() {
     const serviceBusClient = yield* Client
 
@@ -61,18 +61,58 @@ function makeReceiver(queueName: string, waitTillEmpty: Effect<void>, sessionId?
   })
 }
 
+function makeTopicReceiver(
+  topicName: string,
+  subscriptionName: string,
+  waitTillEmpty: Effect<void>,
+  sessionId?: string
+) {
+  return Effect.gen(function*() {
+    const serviceBusClient = yield* Client
+
+    return yield* Effect.acquireRelease(
+      (sessionId
+        ? Effect.promise(() => serviceBusClient.acceptSession(topicName, subscriptionName, sessionId))
+        : Effect.sync(() => serviceBusClient.createReceiver(topicName, subscriptionName)))
+        .pipe(withSpanAndLog(`ServiceBus.receiver.create ${topicName}/${subscriptionName}.${sessionId}`)),
+      (r) =>
+        waitTillEmpty.pipe(
+          withSpanAndLog(`ServiceBus.receiver.waitTillEmpty ${topicName}/${subscriptionName}.${sessionId}`),
+          Effect.andThen(
+            Effect.promise(() => r.close()).pipe(
+              withSpanAndLog(`ServiceBus.receiver.close ${topicName}/${subscriptionName}.${sessionId}`)
+            )
+          ),
+          withSpanAndLog(`ServiceBus.receiver.release ${topicName}/${subscriptionName}.${sessionId}`)
+        )
+    )
+  })
+}
+
 export class ServiceBusReceiverFactory extends Context.TagId(
   "ServiceBusReceiverFactory"
 )<ServiceBusReceiverFactory, {
   make: (waitTillEmpty: Effect<void>) => Effect<ServiceBusReceiver, never, Scope>
   makeSession: (sessionId: string, waitTillEmpty: Effect<void>) => Effect<ServiceBusReceiver, never, Scope>
+  makeTopic: (subscriptionName: string, waitTillEmpty: Effect<void>) => Effect<ServiceBusReceiver, never, Scope>
+  makeTopicSession: (
+    subscriptionName: string,
+    sessionId: string,
+    waitTillEmpty: Effect<void>
+  ) => Effect<ServiceBusReceiver, never, Scope>
 }>() {
   static readonly Live = (queueName: string) =>
     this.toLayer(Client.pipe(Effect.andThen((cl) => ({
       make: (waitTillEmpty: Effect<void>) =>
-        makeReceiver(queueName, waitTillEmpty).pipe(Effect.provideService(Client, cl)),
+        makeQueueReceiver(queueName, waitTillEmpty).pipe(Effect.provideService(Client, cl)),
       makeSession: (sessionId: string, waitTillEmpty: Effect<void>) =>
-        makeReceiver(queueName, waitTillEmpty, sessionId).pipe(Effect.provideService(Client, cl))
+        makeQueueReceiver(queueName, waitTillEmpty, sessionId).pipe(
+          Effect.provideService(Client, cl)
+        ),
+      makeTopic: (subscriptionName: string, waitTillEmpty: Effect<void>) =>
+        makeTopicReceiver(queueName, subscriptionName, waitTillEmpty).pipe(Effect.provideService(Client, cl)),
+      makeTopicSession: (subscriptionName: string, sessionId: string, waitTillEmpty: Effect<void>) =>
+        makeTopicReceiver(queueName, subscriptionName, waitTillEmpty, sessionId).pipe(Effect.provideService(Client, cl))
     }))))
 }
 
@@ -98,12 +138,72 @@ export function subscribe<RMsg, RErr>(hndlr: MessageHandlers<RMsg, RErr>, sessio
         }
         while ((yield* FiberSet.size(fs)) > 0) yield* Effect.sleep("250 millis")
       })
+      .pipe(Effect.delay("10 seconds")) // TODO: just for testing
     const r = yield* sessionId
       ? rf.makeSession(
         sessionId,
         wait
       )
       : rf.make(wait)
+
+    const runEffect = <E>(effect: Effect<void, E, RMsg | RErr>) =>
+      new Promise<void>((resolve, reject) =>
+        fr(effect)
+          .addObserver((exit) => {
+            if (Exit.isSuccess(exit)) {
+              resolve(exit.value)
+            } else {
+              // disable @typescript-eslint/prefer-promise-reject-errors
+              reject(Cause.pretty(exit.cause, { renderErrorCause: true }))
+            }
+          })
+      )
+    yield* Effect.acquireRelease(
+      Effect
+        .sync(() =>
+          r
+            .subscribe({
+              processError: (err) =>
+                runEffect(
+                  hndlr
+                    .processError(err)
+                    .pipe(Effect.catchAllCause((cause) => Effect.logError(`ServiceBus Error ${sessionId}`, cause)))
+                ),
+              processMessage: (msg) => runEffect(hndlr.processMessage(msg))
+              // DO NOT CATCH ERRORS here as they should return to the queue!
+            })
+        )
+        .pipe(withSpanAndLog(`ServiceBus.subscription.create ${sessionId}`)),
+      (subscription) =>
+        Effect.promise(() => subscription.close()).pipe(withSpanAndLog(`ServiceBus.subscription.close ${sessionId}`))
+    )
+  })
+}
+
+export function subscribeTopic<RMsg, RErr>(
+  hndlr: MessageHandlers<RMsg, RErr>,
+  subscriptionName: string,
+  sessionId?: string
+) {
+  return Effect.gen(function*() {
+    const rf = yield* ServiceBusReceiverFactory
+    const fs = yield* FiberSet.make()
+    const fr = yield* FiberSet.runtime(fs)<RMsg | RErr>()
+    const wait = Effect
+      .gen(function*() {
+        if ((yield* FiberSet.size(fs)) > 0) {
+          yield* InfraLogger.logDebug("Waiting ServiceBusFiberSet to be empty: " + (yield* FiberSet.size(fs)))
+        }
+        while ((yield* FiberSet.size(fs)) > 0) yield* Effect.sleep("250 millis")
+      })
+      .pipe(Effect.delay("10 seconds")) // TODO: just for testing
+    const r = yield* sessionId
+      ? rf.makeTopicSession(
+        subscriptionName,
+        sessionId,
+        wait
+      )
+      : rf.makeTopic(subscriptionName, wait)
 
     const runEffect = <E>(effect: Effect<void, E, RMsg | RErr>) =>
       new Promise<void>((resolve, reject) =>

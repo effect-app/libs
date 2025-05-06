a1e4b5b242a8ecbe45f51b6cc5fda3ca67582013
Davide Di Pumpo <davidemak@gmail.com>
2025-05-06 17:16:46 +0200

    Fix/export omega form return (#222)
    * Exports OmegaFormReturn type
    
    * Exports OmegaFormReturn type

--- packages/vue-components/src/components/OmegaForm/index.ts
+++ packages/vue-components/src/components/OmegaForm/index.ts
@@ -1,4 +1,3 @@
-import { defineCustomElement } from "vue"
 import { default as OmegaForm } from "./OmegaWrapper.vue"
 import { default as OmegaInput } from "./OmegaInput.vue"
 import { default as OmegaErrors } from "./OmegaErrors.vue"
@@ -6,7 +5,7 @@ import { default as OmegaAutoGen } from "./OmegaAutoGen.vue"
 
 export * as OmegaErrorsContext from "./OmegaErrorsContext"
 export * from "./OmegaFormStuff"
-export { useOmegaForm } from "./useOmegaForm"
+export { useOmegaForm, type OmegaFormReturn } from "./useOmegaForm"
 export { default } from "./OmegaWrapper.vue"
 
 export { OmegaForm, OmegaInput, OmegaErrors, OmegaAutoGen }

37bfc38f21d459db580e138cca84b8a54624ddb6
Davide Di Pumpo <davidemak@gmail.com>
2025-06-19 15:39:34 +0200

    Fix/vuetify nullable number (#251)
    * fixes vuetifyNullableNumber
    
    * fixes vuetify Nullable Number

--- packages/vue-components/stories/OmegaForm/SimpleForm.vue
+++ packages/vue-components/stories/OmegaForm/SimpleForm.vue
@@ -2,7 +2,7 @@
   <OmegaForm :schema="schema" :on-submit="onSubmit" :subscribe="['values']">
     <template #internalForm="{ form, subscribedValues: { values } }">
       <div>values: {{ values }}</div>
-      <OmegaInput label="asder2" name="asder2" :form="form">
+      <OmegaInput label="asder1" name="asder1" :form="form">
         <template #default="inputProps">
           <label :for="inputProps.name">{{ inputProps.label }}</label>
           <input
@@ -10,10 +10,13 @@
             v-model="inputProps.field.state.value"
             :name="inputProps.name"
             style="border: 1px solid red"
+            type="number"
             @change="(e: any) => inputProps.field.handleChange(e.target.value)"
           />
         </template>
       </OmegaInput>
+      <OmegaInput label="asder2" name="asder2" :form="form" />
+      <OmegaInput label="asder3" name="asder3" :form="form" />
       <button>submit</button>
     </template>
   </OmegaForm>
@@ -23,8 +26,12 @@
 import { S } from "effect-app"
 import { OmegaForm, OmegaInput } from "../../src/components/OmegaForm"
 
-const schema = S.Struct({ asder2: S.String })
-const onSubmit = ({ value }: { value: { asder2: string } }) => {
+const schema = S.Struct({
+  asder2: S.Number,
+  asder1: S.Number,
+  asder3: S.String,
+})
+const onSubmit = ({ value }) => {
   console.log(value)
 }
 </script>

e02a908100dcd270bb88004e15622908d9add1f1
Patrick Roza <contact@patrickroza.com>
2025-07-01 21:30:25 +0200

    bun workaround

--- packages/infra/src/api/internal/events.ts
+++ packages/infra/src/api/internal/events.ts
@@ -1,7 +1,6 @@
 import { Duration, Effect, pipe, S, Schedule, Stream } from "effect-app"
 import { HttpHeaders, HttpServerResponse } from "effect-app/http"
 import { reportError } from "../../errorReporter.js"
-import { InfraLogger } from "../../logger.js"
 import { setupRequestContextFromCurrent } from "../setupRequest.js"
 
 // Tell the client to retry every 10 seconds if connectivity is lost
@@ -17,33 +16,38 @@ export const makeSSE = <A extends { id: any }, SI, SR>(
   Effect
     .gen(function*() {
       const id = connId++
-
-      yield* Effect.annotateCurrentSpan({ connectionId: id.toString() })
-      yield* InfraLogger.logInfo("$ start listening to events, id: " + id.toString())
-      yield* Effect.addFinalizer(() => InfraLogger.logInfo("$ end listening to events, id: " + id.toString()))
-
-      const enc = new TextEncoder()
-
-      const encode = S.encode(schema)
-
-      const eventStream = Stream.flatMap(
-        events,
-        (_) =>
-          encode(_.evt)
-            .pipe(Effect.andThen((evt) => `id: ${_.evt.id}\ndata: ${JSON.stringify(evt)}`))
-      )
-
-      const stream = pipe(
-        setRetry,
-        Stream.merge(keepAlive),
-        Stream.merge(eventStream),
-        Stream.map((_) => enc.encode(_ + "\n\n"))
-      )
-
       const ctx = yield* Effect.context<R | SR>()
       const res = HttpServerResponse.stream(
-        stream
+        // workaround for different scoped behaviour for streams in Bun
+        // https://discord.com/channels/795981131316985866/1098177242598756412/1389646879675125861
+        Effect
+          .gen(function*() {
+            yield* Effect.annotateCurrentSpan({ connectionId: id.toString() })
+            yield* Effect.logInfo("$ start listening to events, id: " + id.toString())
+            yield* Effect.addFinalizer(() => Effect.logInfo("$ end listening to events, id: " + id.toString()))
+
+            const enc = new TextEncoder()
+
+            const encode = S.encode(schema)
+
+            const eventStream = Stream.flatMap(
+              events,
+              (_) =>
+                encode(_.evt)
+                  .pipe(Effect.andThen((evt) => `id: ${_.evt.id}\ndata: ${JSON.stringify(evt)}`))
+            )
+
+            const stream = pipe(
+              setRetry,
+              Stream.merge(keepAlive),
+              Stream.merge(eventStream),
+              Stream.map((_) => enc.encode(_ + "\n\n"))
+            )
+
+            return stream
+          })
           .pipe(
+            Stream.unwrapScoped,
             Stream.tapErrorCause(reportError("Request")),
             Stream.provideContext(ctx)
           ),

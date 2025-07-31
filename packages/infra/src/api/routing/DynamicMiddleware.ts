a9695f62f9e95ead771ad123f934a0d72655c0c8
Patrick Roza <contact@patrickroza.com>
2025-07-31 15:08:13 +0200

    feat: generic middleware

--- packages/infra/src/api/routing/DynamicMiddleware.ts
+++ packages/infra/src/api/routing/DynamicMiddleware.ts
@@ -316,69 +316,74 @@ const reportRequestError = reportError("Request")
 export class DevMode extends Context.Reference<DevMode>()("DevMode", { defaultValue: () => false }) {}
 
 // TODO: pull out to a generic middleware system..
-export const requestMiddleware = <A, E, R>(
-  handle: (input: any, headers: HttpHeaders.Headers) => Effect.Effect<A, E, R>,
-  moduleName: string
-) =>
-  Effect.fnUntraced(function*(input: any, rpcHeaders: HttpHeaders.Headers) {
-    const devMode = yield* DevMode
-    // merge in the request headers
-    // we should consider if we should merge them into rpc headers on the Protocol layer instead.
-    const httpReq = yield* HttpServerRequest.HttpServerRequest
-    const headers = HttpHeaders.merge(httpReq.headers, rpcHeaders)
 
-    return yield* Effect
-      .annotateCurrentSpan(
-        "requestInput",
-        Object.entries(input).reduce((prev, [key, value]: [string, unknown]) => {
-          prev[key] = key === "password"
-            ? "<redacted>"
-            : typeof value === "string" || typeof value === "number" || typeof value === "boolean"
-            ? typeof value === "string" && value.length > 256
-              ? (value.substring(0, 253) + "...")
-              : value
-            : Array.isArray(value)
-            ? `Array[${value.length}]`
-            : value === null || value === undefined
-            ? `${value}`
-            : typeof value === "object" && value
-            ? `Object[${Object.keys(value).length}]`
-            : typeof value
-          return prev
-        }, {} as Record<string, string | number | boolean>)
-      )
-      .pipe(
-        // can't use andThen due to some being a function and effect
-        Effect.zipRight(handle(input, headers)),
-        // TODO: support ParseResult if the error channel of the request allows it.. but who would want that?
-        Effect.catchAll((_) => ParseResult.isParseError(_) ? Effect.die(_) : Effect.fail(_)),
-        Effect.tapErrorCause((cause) => Cause.isFailure(cause) ? logRequestError(cause) : Effect.void),
-        Effect.tapDefect((cause) =>
-          Effect
-            .all([
-              reportRequestError(cause, {
-                action: `${moduleName}.${input._tag}`
-              }),
-              InfraLogger
-                .logError("Finished request", cause)
-                .pipe(Effect.annotateLogs({
-                  action: `${moduleName}.${input._tag}`,
-                  req: pretty(input),
-                  headers: pretty(headers)
-                  // resHeaders: pretty(
-                  //   Object
-                  //     .entries(headers)
-                  //     .reduce((prev, [key, value]) => {
-                  //       prev[key] = value && typeof value === "string" ? snipString(value) : value
-                  //       return prev
-                  //     }, {} as Record<string, any>)
-                  // )
-                }))
-            ])
-        ),
-        devMode ? (_) => _ : Effect.catchAllDefect(() => Effect.die("Internal Server Error"))
-      )
+export class MiddlewareLogger extends Effect.Service<MiddlewareLogger>()("MiddlewareLogger", {
+  effect: Effect.gen(function*() {
+    return <A, E, R>(
+      handle: (input: any, headers: HttpHeaders.Headers) => Effect.Effect<A, E, R>,
+      moduleName: string
+    ) =>
+      Effect.fnUntraced(function*(input: any, rpcHeaders: HttpHeaders.Headers) {
+        const devMode = yield* DevMode
+        // merge in the request headers
+        // we should consider if we should merge them into rpc headers on the Protocol layer instead.
+        const httpReq = yield* HttpServerRequest.HttpServerRequest
+        const headers = HttpHeaders.merge(httpReq.headers, rpcHeaders)
+
+        return yield* Effect
+          .annotateCurrentSpan(
+            "requestInput",
+            Object.entries(input).reduce((prev, [key, value]: [string, unknown]) => {
+              prev[key] = key === "password"
+                ? "<redacted>"
+                : typeof value === "string" || typeof value === "number" || typeof value === "boolean"
+                ? typeof value === "string" && value.length > 256
+                  ? (value.substring(0, 253) + "...")
+                  : value
+                : Array.isArray(value)
+                ? `Array[${value.length}]`
+                : value === null || value === undefined
+                ? `${value}`
+                : typeof value === "object" && value
+                ? `Object[${Object.keys(value).length}]`
+                : typeof value
+              return prev
+            }, {} as Record<string, string | number | boolean>)
+          )
+          .pipe(
+            // can't use andThen due to some being a function and effect
+            Effect.zipRight(handle(input, headers)),
+            // TODO: support ParseResult if the error channel of the request allows it.. but who would want that?
+            Effect.catchAll((_) => ParseResult.isParseError(_) ? Effect.die(_) : Effect.fail(_)),
+            Effect.tapErrorCause((cause) => Cause.isFailure(cause) ? logRequestError(cause) : Effect.void),
+            Effect.tapDefect((cause) =>
+              Effect
+                .all([
+                  reportRequestError(cause, {
+                    action: `${moduleName}.${input._tag}`
+                  }),
+                  InfraLogger
+                    .logError("Finished request", cause)
+                    .pipe(Effect.annotateLogs({
+                      action: `${moduleName}.${input._tag}`,
+                      req: pretty(input),
+                      headers: pretty(headers)
+                      // resHeaders: pretty(
+                      //   Object
+                      //     .entries(headers)
+                      //     .reduce((prev, [key, value]) => {
+                      //       prev[key] = value && typeof value === "string" ? snipString(value) : value
+                      //       return prev
+                      //     }, {} as Record<string, any>)
+                      // )
+                    }))
+                ])
+            ),
+            devMode ? (_) => _ : Effect.catchAllDefect(() => Effect.die("Internal Server Error"))
+          )
+      })
   })
+}) {}
 
 // factory for middlewares
 export const makeMiddleware =
@@ -428,18 +433,19 @@ export const makeMiddleware =
       Effect
         .all({
           dynamicMiddlewares: dynamicMiddlewares.effect,
+          logger: MiddlewareLogger,
           middleware: make.execute((
             cb: MakeRPCHandlerFactory<RequestContextMap, HttpRouter.HttpRouter.Provided | ContextProviderA>
           ) => cb),
           contextProvider: make.contextProvider // uses the middleware.contextProvider tag to get the context provider service
         })
         .pipe(
-          Effect.map(({ contextProvider, dynamicMiddlewares, middleware }) => ({
+          Effect.map(({ contextProvider, dynamicMiddlewares, logger, middleware }) => ({
             _tag: "MiddlewareMaker" as const,
             effect: makeRpcEffect<RequestContextMap, ContextProviderA>()(
               (schema, handler, moduleName) => {
                 const h = middleware(schema, handler as any, moduleName)
-                return requestMiddleware(
+                return logger(
                   Effect.fnUntraced(function*(req, headers) {
                     yield* Effect.annotateCurrentSpan(
                       "request.name",
@@ -470,7 +476,8 @@ export const makeMiddleware =
           Layer.mergeAll(
             make.dependencies ? make.dependencies as any : Layer.empty,
             ...(dynamicMiddlewares.dependencies as any),
-            make.contextProvider.Default
+            make.contextProvider.Default,
+            MiddlewareLogger.Default
           )
         )
       ) as Layer.Layer<

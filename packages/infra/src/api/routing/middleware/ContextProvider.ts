575723cb203ea1083364522848bf96df2c5db428
jfet97 <andrysimo1997@gmail.com>
2025-08-01 17:13:37 +0200

    fix: Handle cases where generator functions do not yield values in EffectGenUtils types

--- packages/infra/src/api/routing/middleware/ContextProvider.ts
+++ packages/infra/src/api/routing/middleware/ContextProvider.ts
@@ -7,16 +7,19 @@ import { type ContextTagWithDefault, type GetContext, type LayerUtils, mergeCont
 
 namespace EffectGenUtils {
   export type Success<EG> = EG extends Effect<infer A, infer _E, infer _R> ? A
-    : EG extends (..._: infer _3) => Generator<YieldWrap<Effect<infer _, infer _E, infer _R>>, infer A, infer _2> ? A
+    // there could be a case where the generator function does not yield anything, so we need to handle that
     : EG extends (..._: infer _3) => Generator<never, infer A, infer _2> ? A
+    : EG extends (..._: infer _3) => Generator<YieldWrap<Effect<infer _, infer _E, infer _R>>, infer A, infer _2> ? A
     : never
 
   export type Error<EG> = EG extends Effect<infer _A, infer E, infer _R> ? E
+    // there could be a case where the generator function does not yield anything, so we need to handle that
     : EG extends (..._: infer _3) => Generator<never, infer _A, infer _2> ? never
     : EG extends (..._: infer _3) => Generator<YieldWrap<Effect<infer _, infer E, infer _R>>, infer _A, infer _2> ? E
     : never
 
   export type Context<EG> = EG extends Effect<infer _A, infer _E, infer R> ? R
+    // there could be a case where the generator function does not yield anything, so we need to handle that
     : EG extends (..._: infer _3) => Generator<never, infer _A, infer _2> ? never
     : EG extends (..._: infer _3) => Generator<YieldWrap<Effect<infer _, infer _E, infer R>>, infer _A, infer _2> ? R
     : never

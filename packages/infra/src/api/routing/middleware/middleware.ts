ce16bc8d5a45d368ab9d6675e6f150cb73746cc4
Patrick Roza <contact@patrickroza.com>
2025-08-07 18:10:37 +0200

    extract tags from implementation

--- packages/infra/src/api/routing/middleware/middleware.ts
+++ packages/infra/src/api/routing/middleware/middleware.ts
@@ -1,13 +1,15 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { Cause, Context, Duration, Effect, Layer, ParseResult, Request, Schedule, type Schema } from "effect-app"
+import { Cause, Duration, Effect, Layer, ParseResult, Request, Schedule, type Schema, Unify } from "effect"
 import { pretty } from "effect-app/utils"
+import { type TagUnify, type TagUnifyIgnore } from "effect/Context"
 import { logError, reportError } from "../../../errorReporter.js"
 import { InfraLogger } from "../../../logger.js"
 import { determineMethod, isCommand } from "../utils.js"
-import { TagService } from "./RpcMiddlewareX.js"
+import * as MiddlewareNative from "./middleware-native.js"
 
 const logRequestError = logError("Request")
 const reportRequestError = reportError("Request")
+
+export { DevMode } from "./middleware-native.js"
 
 export const RequestCacheLayers = Layer.mergeAll(
   Layer.setRequestCache(
@@ -17,13 +19,14 @@ export const RequestCacheLayers = Layer.mergeAll(
   Layer.setRequestBatching(true)
 )
 
-export class DevMode extends Context.Reference<DevMode>()("DevMode", { defaultValue: () => false }) {}
-
-export class RequestCacheMiddleware
-  extends TagService<RequestCacheMiddleware>()("RequestCacheMiddleware", { wrap: true })({
-    effect: Effect.succeed(({ next }) => next.pipe(Effect.provide(RequestCacheLayers)))
-  })
-{
+export class RequestCacheMiddleware extends MiddlewareNative.RequestCacheMiddleware {
+  static readonly Default = Layer.succeed(
+    this,
+    ({ next }) => next.pipe(Effect.provide(RequestCacheLayers))
+  )
+  static override [Unify.typeSymbol]?: unknown
+  static override [Unify.unifySymbol]?: TagUnify<typeof this>
+  static override [Unify.ignoreSymbol]?: TagUnifyIgnore
 }
 
 // retry just once on optimistic concurrency exceptions
@@ -31,9 +34,10 @@ const optimisticConcurrencySchedule = Schedule.once.pipe(
   Schedule.intersect(Schedule.recurWhile<any>((a) => a?._tag === "OptimisticConcurrencyException"))
 )
 
-export class ConfigureInterruptibilityMiddleware
-  extends TagService<ConfigureInterruptibilityMiddleware>()("ConfigureInterruptibilityMiddleware", { wrap: true })({
-    effect: Effect.gen(function*() {
+export class ConfigureInterruptibilityMiddleware extends MiddlewareNative.ConfigureInterruptibilityMiddleware {
+  static readonly Default = Layer.effect(
+    MiddlewareNative.RequestCacheMiddleware,
+    Effect.gen(function*() {
       const cache = new Map()
       const getCached = (key: string, schema: Schema.Schema.Any) => {
         const existing = cache.get(key)
@@ -52,69 +56,76 @@ export class ConfigureInterruptibilityMiddleware
         return next
       }
     })
-  })
-{
+  )
+  static override [Unify.typeSymbol]?: unknown
+  static override [Unify.unifySymbol]?: TagUnify<typeof this>
+  static override [Unify.ignoreSymbol]?: TagUnifyIgnore
 }
 
-export class LoggerMiddleware extends TagService<LoggerMiddleware>()("LoggerMiddleware", { wrap: true })({
-  effect: Effect.gen(function*() {
-    const devMode = yield* DevMode
-    return ({ headers, next, payload, rpc }) =>
-      Effect
-        .annotateCurrentSpan({
-          "request.name": rpc._tag,
-          "requestInput": typeof payload === "object" && payload !== null
-            ? Object.entries(payload).reduce((prev, [key, value]: [string, unknown]) => {
-              prev[key] = key === "password"
-                ? "<redacted>"
-                : typeof value === "string" || typeof value === "number" || typeof value === "boolean"
-                ? typeof value === "string" && value.length > 256
-                  ? (value.substring(0, 253) + "...")
-                  : value
-                : Array.isArray(value)
-                ? `Array[${value.length}]`
-                : value === null || value === undefined
-                ? `${value}`
-                : typeof value === "object" && value
-                ? `Object[${Object.keys(value).length}]`
-                : typeof value
-              return prev
-            }, {} as Record<string, string | number | boolean>)
-            : payload
-        })
-        .pipe(
-          // can't use andThen due to some being a function and effect
-          Effect.zipRight(next),
-          // TODO: support ParseResult if the error channel of the request allows it.. but who would want that?
-          Effect.catchAll((_) => ParseResult.isParseError(_) ? Effect.die(_) : Effect.fail(_)),
-          Effect.tapErrorCause((cause) => Cause.isFailure(cause) ? logRequestError(cause) : Effect.void),
-          Effect.tapDefect((cause) =>
-            Effect
-              .all([
-                reportRequestError(cause, {
-                  action: rpc._tag
-                }),
-                InfraLogger
-                  .logError("Finished request", cause)
-                  .pipe(Effect.annotateLogs({
-                    action: rpc._tag,
-                    req: pretty(payload),
-                    headers: pretty(headers)
-                    // resHeaders: pretty(
-                    //   Object
-                    //     .entries(headers)
-                    //     .reduce((prev, [key, value]) => {
-                    //       prev[key] = value && typeof value === "string" ? snipString(value) : value
-                    //       return prev
-                    //     }, {} as Record<string, any>)
-                    // )
-                  }))
-              ])
-          ),
-          devMode ? (_) => _ : Effect.catchAllDefect(() => Effect.die("Internal Server Error"))
-        )
-  })
-}) {
+export class LoggerMiddleware extends MiddlewareNative.LoggerMiddleware {
+  static readonly Default = Layer.effect(
+    this,
+    Effect.gen(function*() {
+      const devMode = yield* MiddlewareNative.DevMode
+      return ({ headers, next, payload, rpc }) =>
+        Effect
+          .annotateCurrentSpan({
+            "request.name": rpc._tag,
+            "requestInput": typeof payload === "object" && payload !== null
+              ? Object.entries(payload).reduce((prev, [key, value]: [string, unknown]) => {
+                prev[key] = key === "password"
+                  ? "<redacted>"
+                  : typeof value === "string" || typeof value === "number" || typeof value === "boolean"
+                  ? typeof value === "string" && value.length > 256
+                    ? (value.substring(0, 253) + "...")
+                    : value
+                  : Array.isArray(value)
+                  ? `Array[${value.length}]`
+                  : value === null || value === undefined
+                  ? `${value}`
+                  : typeof value === "object" && value
+                  ? `Object[${Object.keys(value).length}]`
+                  : typeof value
+                return prev
+              }, {} as Record<string, string | number | boolean>)
+              : payload
+          })
+          .pipe(
+            // can't use andThen due to some being a function and effect
+            Effect.zipRight(next),
+            // TODO: support ParseResult if the error channel of the request allows it.. but who would want that?
+            Effect.catchAll((_) => ParseResult.isParseError(_) ? Effect.die(_) : Effect.fail(_)),
+            Effect.tapErrorCause((cause) => Cause.isFailure(cause) ? logRequestError(cause) : Effect.void),
+            Effect.tapDefect((cause) =>
+              Effect
+                .all([
+                  reportRequestError(cause, {
+                    action: rpc._tag
+                  }),
+                  InfraLogger
+                    .logError("Finished request", cause)
+                    .pipe(Effect.annotateLogs({
+                      action: rpc._tag,
+                      req: pretty(payload),
+                      headers: pretty(headers)
+                      // resHeaders: pretty(
+                      //   Object
+                      //     .entries(headers)
+                      //     .reduce((prev, [key, value]) => {
+                      //       prev[key] = value && typeof value === "string" ? snipString(value) : value
+                      //       return prev
+                      //     }, {} as Record<string, any>)
+                      // )
+                    }))
+                ])
+            ),
+            devMode ? (_) => _ : Effect.catchAllDefect(() => Effect.die("Internal Server Error"))
+          )
+    })
+  )
+  static override [Unify.typeSymbol]?: unknown
+  static override [Unify.unifySymbol]?: TagUnify<typeof this>
+  static override [Unify.ignoreSymbol]?: TagUnifyIgnore
 }
 
 export const DefaultGenericMiddlewares = [

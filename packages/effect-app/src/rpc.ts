edb6bf94db5e3709c179c01cc50ee8ca0637589b
Patrick Roza <contact@patrickroza.com>
2025-08-15 12:39:04 +0200

    cleanup

--- packages/effect-app/src/rpc.ts
+++ packages/effect-app/src/rpc.ts
@@ -2,7 +2,6 @@
 export * from "./rpc/MiddlewareMaker.js"
 export * from "./rpc/RpcContextMap.js"
 export * from "./rpc/RpcMiddleware.js"
-export * from "./rpc/RpcX.js"
 // codegen:end
 
 export * as Middleware from "./rpc.js"
--- packages/effect-app/src/rpc/RpcMiddleware.ts
+++ packages/effect-app/src/rpc/RpcMiddleware.ts
@@ -3,11 +3,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { type Rpc, RpcMiddleware } from "@effect/rpc"
 import { type SuccessValue, type TypeId } from "@effect/rpc/RpcMiddleware"
-import { type Context, type Effect, type Schema, type Schema as S, type Scope, Unify } from "effect"
+import { type Context, type Effect, type Schema, type Schema as S, type Scope, type Stream, Unify } from "effect"
 import { type HttpHeaders } from "effect-app/http"
 import { type NonEmptyReadonlyArray } from "effect/Array"
 import { type TagUnify, type TagUnifyIgnore } from "effect/Context"
-import { type RpcContextMap } from "./RpcContextMap.js"
+import { type ReadonlyMailbox } from "effect/Mailbox"
+import { type GetEffectContext, type RpcContextMap } from "./RpcContextMap.js"
 
 // updated to support Scope.Scope and follow V4: Provides/Requires as Identifiers instead of Tag, wrap is default
 export interface RpcMiddlewareV4<Provides, E, Requires> {
@@ -191,3 +192,73 @@ export const Tag = <
     static override [Unify.unifySymbol]?: TagUnify<typeof this>
     static override [Unify.ignoreSymbol]?: TagUnifyIgnore
   } as any
+
+// not needed if there's official support in Rpc.Rpc.
+export type AddMiddleware<R extends Rpc.Any, Middleware extends RpcMiddleware.TagClassAny> = R extends Rpc.Rpc<
+  infer _Tag,
+  infer _Payload,
+  infer _Success,
+  infer _Error,
+  infer _Middleware
+> ?
+    & Rpc.Rpc<
+      _Tag,
+      _Payload,
+      _Success,
+      _Error,
+      _Middleware | Middleware
+    >
+    & { readonly config: R extends { readonly config: infer _C } ? _C : never }
+  : never
+
+// customized versions to handle dynamically eliminated context.
+export type HandlersContext<Rpcs extends Rpc.Any, Handlers> = keyof Handlers extends infer K
+  ? K extends keyof Handlers & string ? HandlerContext<Rpcs, K, Handlers[K]> : never
+  : never
+
+export type HandlerContext<Rpcs extends Rpc.Any, K extends Rpcs["_tag"], Handler> = [Rpc.IsStream<Rpcs, K>] extends
+  [true] ? Handler extends (...args: any) =>
+    | Stream.Stream<infer _A, infer _E, infer _R>
+    | Rpc.Fork<Stream.Stream<infer _A, infer _E, infer _R>>
+    | Effect.Effect<
+      ReadonlyMailbox<infer _A, infer _E>,
+      infer _EX,
+      infer _R
+    >
+    | Rpc.Fork<
+      Effect.Effect<
+        ReadonlyMailbox<infer _A, infer _E>,
+        infer _EX,
+        infer _R
+      >
+    > ? Exclude<ExcludeProvides<_R, Rpcs, K>, Scope.Scope>
+  : never
+  : Handler extends (
+    ...args: any
+  ) => Effect.Effect<infer _A, infer _E, infer _R> | Rpc.Fork<Effect.Effect<infer _A, infer _E, infer _R>>
+    ? ExcludeProvides<_R, Rpcs, K>
+  : never
+
+// new
+export type ExtractDynamicallyProvides<R extends Rpc.Any, Tag extends string> = R extends
+  Rpc.Rpc<Tag, infer _Payload, infer _Success, infer _Error, infer _Middleware> ? _Middleware extends {
+    readonly requestContextMap: infer _RC
+  } ? _RC extends Record<string, RpcContextMap.Any> // ? GetEffectContext<_RC, { allowAnonymous: false }>
+      ? R extends { readonly config: infer _C } ? GetEffectContext<_RC, _C>
+      : GetEffectContext<_RC, {}>
+    : never
+  : never
+  : never
+
+export type ExtractProvides<R extends Rpc.Any, Tag extends string> = R extends
+  Rpc.Rpc<Tag, infer _Payload, infer _Success, infer _Error, infer _Middleware> ? _Middleware extends {
+    readonly provides: Context.Tag<infer _I, infer _S>
+  } ? _I
+  : never
+  : never
+
+export type ExcludeProvides<Env, R extends Rpc.Any, Tag extends string> = Exclude<
+  Env,
+  // customisation is down here.
+  ExtractProvides<R, Tag> | ExtractDynamicallyProvides<R, Tag>
+>

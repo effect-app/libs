66adb8d1d47f07537600af4359fac92212a4e191
jfet97 <andrysimo1997@gmail.com>
2025-08-17 19:41:58 +0200

    test: Add additional test cases for pipeGen functionality

--- packages/infra/test/pipegen.test.ts
+++ packages/infra/test/pipegen.test.ts
@@ -2,6 +2,10 @@ import { expect, expectTypeOf, it } from "@effect/vitest"
 import { Effect } from "effect-app"
 import { pipeGen } from "../src/pipegen.js"
 
+const test = pipeGen(function*() {
+  return 5
+})
+
 it(
   "works",
   Effect.fnUntraced(function*() {
@@ -36,5 +40,18 @@ it(
     )
     expectTypeOf(res3).toEqualTypeOf<Effect.Effect<boolean>>()
     expect(yield* res3).toEqual(false)
+
+    const res4 = pipeGen(
+      function*() {
+        // note: no yield* of effects here
+        return 8
+      },
+      Effect.map((n) => String(n + 1)),
+      function*(e) {
+        return (yield* e).repeat(2).length > 3
+      }
+    )
+    expectTypeOf(res4).toEqualTypeOf<Effect.Effect<boolean>>()
+    expect(yield* res4).toEqual(false)
   }, Effect.runPromise)
 )

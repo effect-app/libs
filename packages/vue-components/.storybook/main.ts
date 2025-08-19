e3b1b4c602eb7df02222dace882445ac55e6e149
Davide Di Pumpo <davidemak@gmail.com>
2025-08-19 19:57:55 +0200

    updates storybook (#320)

--- packages/vue-components/.storybook/main.ts
+++ packages/vue-components/.storybook/main.ts
@@ -5,17 +5,10 @@ const config: StorybookConfig = {
     "../stories/**/*.mdx",
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  addons: [],
   framework: {
     name: "@storybook/vue3-vite",
     options: {},
-  },
-  docs: {
-    autodocs: "tag",
   },
 }
 export default config

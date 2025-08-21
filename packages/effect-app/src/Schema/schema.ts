5755d87fc823624b6c5890e13d30617c20465fdb
Patrick Roza <contact@patrickroza.com>
2025-08-21 22:58:29 +0200

    cleanup

--- packages/effect-app/src/Schema/schema.ts
+++ packages/effect-app/src/Schema/schema.ts
@@ -1,5 +1,5 @@
 import * as A from "effect/Arbitrary"
-import type * as B from "effect/Brand"
+import * as B from "effect/Brand"
 import * as P from "effect/ParseResult"
 import * as AST from "effect/SchemaAST"
 

51b36003c1d48cad9029e76b02423e36fe23ec85
Patrick Roza <contact@patrickroza.com>
2025-08-22 15:36:07 +0200

    remove namespace/type merging in Prelude.

---
+++ .changeset/slick-dryers-brush.md
@@ -0,0 +1,5 @@
+---
+"effect-app": minor
+---
+
+remove namespace/type merging in Prelude.
--- packages/effect-app/src/index.ts
+++ packages/effect-app/src/index.ts
@@ -1,3 +1,22 @@
 import "./builtin.js"
 
-export * from "./internal/lib.js"
+export * as Fnc from "./Function.js"
+export * as Utils from "./utils.js"
+
+export * as Array from "./Array.js"
+export * as Context from "./Context.js"
+export * as Effect from "./Effect.js"
+export * as Layer from "./Layer.js"
+export * as NonEmptySet from "./NonEmptySet.js"
+export * as Set from "./Set.js"
+
+export { type NonEmptyArray, type NonEmptyReadonlyArray } from "./Array.js"
+
+export * from "effect"
+
+export * as Struct from "./Struct.js"
+export type * as Types from "./Types.js"
+
+export * as SecretURL from "./Config/SecretURL.js"
+export * as S from "./Schema.js"
+export { copy } from "./utils.js"

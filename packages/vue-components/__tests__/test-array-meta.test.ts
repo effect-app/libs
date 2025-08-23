d00f8c625b23f50df9b92254668a709818ef490f
Davide Di Pumpo <davidemak@gmail.com>
2025-08-23 03:04:16 +0200

    Fix/nested arrays (#339)
    * Added support for generating metadata for nullable and nested array structures.
    
    * changeset

--- packages/vue-components/__tests__/test-array-meta.test.ts
+++ packages/vue-components/__tests__/test-array-meta.test.ts
@@ -11,12 +11,29 @@ const schema = S.Struct({
     e: S.Array(S.Number),
   })),
   aMultiple: S.Array(S.String),
+  aNullable: S.NullOr(S.Struct({
+    a: S.String,
+  })),
+  foo: S
+    .NullOr(
+      S.Struct({
+        bar: S.UndefinedOr(S.Array(S.Struct({
+          baz: S.String,
+          items: S.NonEmptyArray(S.Struct({
+            baz: S.String,
+            value: S.Number
+          }))
+        }))),
+      })
+    ),
 })
 
 describe("test-array-meta", () => {
   it("should generate metadata for arrays", () => {
     const result = generateMetaFromSchema(schema)
     console.log("Generated metadata:", JSON.stringify(result.meta, null, 2))
+    console.log("Keys:", Object.keys(result.meta))
+    
 
     expect(result.meta).toHaveProperty("a.b")
     expect(result.meta["a.b"].type).toBe("string")
@@ -26,5 +43,14 @@ describe("test-array-meta", () => {
     expect(result.meta["a.e"].type).toBe("multiple")
     expect(result.meta).toHaveProperty("aMultiple")
     expect(result.meta["aMultiple"]?.type).toBe("multiple")
+    expect(result.meta).toHaveProperty("aNullable.a")
+    expect(result.meta["aNullable.a"]?.type).toBe("string")
+    expect(result.meta).not.toHaveProperty("aNullable")
+    expect(result.meta).toHaveProperty("foo.bar.items.baz")
+    expect(result.meta["foo.bar.items.baz"]?.type).toBe("string")
+    expect(result.meta).toHaveProperty("foo.bar.items.value")
+    expect(result.meta["foo.bar.items.value"]?.type).toBe("number")
+    expect(result.meta).toHaveProperty("foo.bar.baz")
+    expect(result.meta["foo.bar.baz"]?.type).toBe("string")
   })
 })

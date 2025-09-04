8f710e64c8f4c12a2a6a1d7eef4b36a0e1f1d537
jfet97 <andrysimo1997@gmail.com>
2025-09-04 12:48:58 +0200

    wip

--- packages/vue-components/eslint.config.mjs
+++ packages/vue-components/eslint.config.mjs
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default [
-  ...vueConfig(__dirname, false),
+  ...vueConfig(__dirname, false, path.join(__dirname, "dprint.json")),
   {
     ignores: [".output/**", "dist/**", "node_modules/**"],
   },

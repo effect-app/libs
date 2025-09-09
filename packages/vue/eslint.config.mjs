278861fb1d133621d40c81d9dcc8dc29fc36d3ff
jfet97 <andrysimo1997@gmail.com>
2025-09-09 12:35:40 +0200

    fix: enable warning for unnecessary type assertions in ESLint config

--- packages/vue/eslint.config.mjs
+++ packages/vue/eslint.config.mjs
@@ -19,6 +19,7 @@ export default [
     rules: {
       "@typescript-eslint/no-empty-interface": "off",
       "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unnecessary-type-assertion": "warn",
     }
   }
 ]

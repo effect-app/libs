56fc50792e9539ca916348afae3d1310a5755076
jfet97 <andrysimo1997@gmail.com>
2025-09-09 15:14:46 +0200

    Refactor code structure for improved readability and maintainability

--- packages/eslint-shared-config/src/eslint.vue.config.mjs
+++ packages/eslint-shared-config/src/eslint.vue.config.mjs
@@ -1,19 +1,26 @@
 import formatjs from "eslint-plugin-formatjs"
 import pluginVue from "eslint-plugin-vue"
 import { defineConfigWithVueTs, vueTsConfigs } from "@vue/eslint-config-typescript"
-
 import tseslint from "typescript-eslint"
-
 import { baseConfig } from "./eslint.base.config.mjs"
-
 import dprint from "@ben_12/eslint-plugin-dprint"
+import { fileURLToPath } from "node:url"
+import path from "node:path"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const DEFAULT_DPRINT_CONFIG = path.join(__dirname, "dprint.json")
 
 /**
  * @param {string} dirName
  * @param {boolean} [forceTS=false]
  * @returns {import("eslint").Linter.FlatConfig[]}
  */
-export function vueConfig(dirName, forceTS = false, dprintConfigFile = "dprint.json") {
+export function vueConfig(dirName, forceTS = false, dprintConfigFile ) {
+
+  if (!dprintConfigFile) dprintConfigFile = DEFAULT_DPRINT_CONFIG
+  console.log("Using dprint config file:", dprintConfigFile)
+
   const enableTS = !!dirName && (forceTS || process.env["ESLINT_TS"])
 
   return [
@@ -83,27 +90,21 @@ export function vueConfig(dirName, forceTS = false, dprintConfigFile = "dprint.j
         ],
         "@ben_12/dprint/typescript": [
           "error",
-          {
-            // Use dprint JSON configuration file (default: "dprint.json")
-            // It may be created using `dprint init` command
-            // See also https://dprint.dev/config/
-            configFile: "dprint.json",
-            // The TypeScript configuration of dprint
-            // See also https://dprint.dev/plugins/typescript/config/
-            config: {
-              // The TypeScript configuration of dprint
-              // See also https://dprint.dev/plugins/typescript/config/,
-              indentWidth: 2,
-              semiColons: "asi",
-              quoteStyle: "alwaysDouble",
-              trailingCommas: "never",
-              "arrowFunction.useParentheses": "force",
-              "memberExpression.linePerExpression": true,
-              "binaryExpression.linePerExpression": true,
-              "importDeclaration.forceSingleLine": true,
-              "exportDeclaration.forceSingleLine": true,
+            {
+              // Usa lo stesso file config risolto in modo assoluto
+              configFile: dprintConfigFile,
+              config: {
+                indentWidth: 2,
+                semiColons: "asi",
+                quoteStyle: "alwaysDouble",
+                trailingCommas: "never",
+                "arrowFunction.useParentheses": "force",
+                "memberExpression.linePerExpression": true,
+                "binaryExpression.linePerExpression": true,
+                "importDeclaration.forceSingleLine": true,
+                "exportDeclaration.forceSingleLine": true,
+              },
             },
-          },
         ],
       },
     },

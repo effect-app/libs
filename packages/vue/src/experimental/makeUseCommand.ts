8d5a32019da17609b62fb82b175c5a9f24552b19
Patrick Roza <contact@patrickroza.com>
2025-09-11 16:28:39 +0200

    feat: alt2 experiment (#420)
    * alt 2 baby
    
    * fixy
    
    * cleanup

--- packages/vue/src/experimental/makeUseCommand.ts
+++ packages/vue/src/experimental/makeUseCommand.ts
@@ -5,5 +5,11 @@ export const makeUseCommand = Effect.fnUntraced(function*<R = never>() {
   const cmndr = yield* Commander
   const runtime = yield* Effect.runtime<R>()
 
-  return { ...cmndr, alt: cmndr.alt(runtime), fn: cmndr.fn(runtime), wrap: cmndr.wrap(runtime) }
+  return {
+    ...cmndr,
+    alt: cmndr.alt(runtime),
+    fn: cmndr.fn(runtime),
+    wrap: cmndr.wrap(runtime),
+    alt2: cmndr.alt2(runtime)
+  }
 })

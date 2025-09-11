5f4a178130969e3c22f62fc1025ef77b1a200663
Patrick Roza <contact@patrickroza.com>
2025-09-11 17:25:46 +0200

    fix provide devmode

--- packages/infra/src/api/routing/middleware/middleware.ts
+++ packages/infra/src/api/routing/middleware/middleware.ts
@@ -9,6 +9,15 @@ import { determineMethod, isCommand } from "../utils.js"
 const logRequestError = logError("Request")
 const reportRequestError = reportError("Request")
 
+// TODO: do we need this as middleware or just as layer?
+export const DevModeLive = Layer.effect(
+  DevMode,
+  Effect.gen(function*() {
+    const env = yield* Config.string("env").pipe(Config.withDefault("local-dev"))
+    return env !== "prod"
+  })
+)
+
 export const RequestCacheLayers = Layer.mergeAll(
   Layer.setRequestCache(
     Request.makeCache({ capacity: 500, timeToLive: Duration.hours(8) })
@@ -50,75 +59,69 @@ export const ConfigureInterruptibilityMiddlewareLive = Layer.effect(
   })
 )
 
-export const LoggerMiddlewareLive = Layer.effect(
-  LoggerMiddleware,
-  Effect.gen(function*() {
-    const devMode = yield* DevMode
-    return (effect, { headers, payload, rpc }) =>
-      Effect
-        .annotateCurrentSpan({
-          "request.name": rpc._tag,
-          "requestInput": typeof payload === "object" && payload !== null
-            ? Object.entries(payload).reduce((prev, [key, value]: [string, unknown]) => {
-              prev[key] = key === "password"
-                ? "<redacted>"
-                : typeof value === "string" || typeof value === "number" || typeof value === "boolean"
-                ? typeof value === "string" && value.length > 256
-                  ? (value.substring(0, 253) + "...")
-                  : value
-                : Array.isArray(value)
-                ? `Array[${value.length}]`
-                : value === null || value === undefined
-                ? `${value}`
-                : typeof value === "object" && value
-                ? `Object[${Object.keys(value).length}]`
-                : typeof value
-              return prev
-            }, {} as Record<string, string | number | boolean>)
-            : payload
-        })
-        .pipe(
-          // can't use andThen due to some being a function and effect
-          Effect.zipRight(effect),
-          // TODO: support ParseResult if the error channel of the request allows it.. but who would want that?
-          Effect.catchAll((_) => ParseResult.isParseError(_) ? Effect.die(_) : Effect.fail(_)),
-          Effect.tapErrorCause((cause) => Cause.isFailure(cause) ? logRequestError(cause) : Effect.void),
-          Effect.tapDefect((cause) =>
-            Effect
-              .all([
-                reportRequestError(cause, {
-                  action: rpc._tag
-                }),
-                InfraLogger
-                  .logError("Finished request", cause)
-                  .pipe(Effect.annotateLogs({
-                    action: rpc._tag,
-                    req: pretty(payload),
-                    headers: pretty(headers)
-                    // resHeaders: pretty(
-                    //   Object
-                    //     .entries(headers)
-                    //     .reduce((prev, [key, value]) => {
-                    //       prev[key] = value && typeof value === "string" ? snipString(value) : value
-                    //       return prev
-                    //     }, {} as Record<string, any>)
-                    // )
-                  }))
-              ])
-          ),
-          devMode ? (_) => _ : Effect.catchAllDefect(() => Effect.die("Internal Server Error"))
-        )
-  })
-)
+export const LoggerMiddlewareLive = Layer
+  .effect(
+    LoggerMiddleware,
+    Effect.gen(function*() {
+      const devMode = yield* DevMode
+      return (effect, { headers, payload, rpc }) =>
+        Effect
+          .annotateCurrentSpan({
+            "request.name": rpc._tag,
+            "requestInput": typeof payload === "object" && payload !== null
+              ? Object.entries(payload).reduce((prev, [key, value]: [string, unknown]) => {
+                prev[key] = key === "password"
+                  ? "<redacted>"
+                  : typeof value === "string" || typeof value === "number" || typeof value === "boolean"
+                  ? typeof value === "string" && value.length > 256
+                    ? (value.substring(0, 253) + "...")
+                    : value
+                  : Array.isArray(value)
+                  ? `Array[${value.length}]`
+                  : value === null || value === undefined
+                  ? `${value}`
+                  : typeof value === "object" && value
+                  ? `Object[${Object.keys(value).length}]`
+                  : typeof value
+                return prev
+              }, {} as Record<string, string | number | boolean>)
+              : payload
+          })
+          .pipe(
+            // can't use andThen due to some being a function and effect
+            Effect.zipRight(effect),
+            // TODO: support ParseResult if the error channel of the request allows it.. but who would want that?
+            Effect.catchAll((_) => ParseResult.isParseError(_) ? Effect.die(_) : Effect.fail(_)),
+            Effect.tapErrorCause((cause) => Cause.isFailure(cause) ? logRequestError(cause) : Effect.void),
+            Effect.tapDefect((cause) =>
+              Effect
+                .all([
+                  reportRequestError(cause, {
+                    action: rpc._tag
+                  }),
+                  InfraLogger
+                    .logError("Finished request", cause)
+                    .pipe(Effect.annotateLogs({
+                      action: rpc._tag,
+                      req: pretty(payload),
+                      headers: pretty(headers)
+                      // resHeaders: pretty(
+                      //   Object
+                      //     .entries(headers)
+                      //     .reduce((prev, [key, value]) => {
+                      //       prev[key] = value && typeof value === "string" ? snipString(value) : value
+                      //       return prev
+                      //     }, {} as Record<string, any>)
+                      // )
+                    }))
+                ])
+            ),
+            devMode ? (_) => _ : Effect.catchAllDefect(() => Effect.die("Internal Server Error"))
+          )
+    })
+  )
+  .pipe(Layer.provide(DevModeLive))
 
-// TODO: do we need this as middleware or just as layer?
-export const DevModeLive = Layer.effect(
-  DevMode,
-  Effect.gen(function*() {
-    const env = yield* Config.string("env").pipe(Config.withDefault("local-dev"))
-    return env !== "prod"
-  })
-)
 export const DevModeMiddlewareLive = Layer
   .effect(
     DevModeMiddleware,

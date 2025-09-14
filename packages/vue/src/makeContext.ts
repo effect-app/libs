6272279c54b22957eaa0621a8ad50f3cfd1fa59c
Patrick Roza <contact@patrickroza.com>
2025-09-14 11:35:00 +0200

    expand inject helpers

--- packages/vue/src/makeContext.ts
+++ packages/vue/src/makeContext.ts
@@ -1,9 +1,34 @@
 import { inject, type InjectionKey, provide } from "vue"
 
-export const makeContext = <T>(def: T) => {
+export const makeContext: {
+  <T>(def: T): {
+    use: () => T
+    provide: (locale: T) => void
+  }
+  <T>(def?: T | undefined): {
+    use: () => T | undefined
+    provide: (locale: T) => void
+  }
+} = <T>(def?: T) => {
   const key = Symbol() as InjectionKey<T>
   return {
     use: () => inject(key, def),
+    provide: (locale: T) => provide(key, locale)
+  }
+}
+
+export const injectCertain = <T>(key: InjectionKey<T>) => {
+  const v = inject(key)
+  if (v === undefined) {
+    throw new Error(`Injectionkey ${key.toString()} not found`)
+  }
+  return v
+}
+
+export const makeContextCertain = <T>() => {
+  const key = Symbol() as InjectionKey<T>
+  return {
+    use: () => injectCertain(key),
     provide: (locale: T) => provide(key, locale)
   }
 }

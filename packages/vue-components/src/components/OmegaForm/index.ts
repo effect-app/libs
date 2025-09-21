b6c7b9dba7d2168be13e584e3785133eb4bb376f
Patrick Roza <contact@patrickroza.com>
2025-09-21 07:52:54 +0000

    cleanup exports

--- packages/vue-components/src/components/OmegaForm/index.ts
+++ packages/vue-components/src/components/OmegaForm/index.ts
@@ -1,7 +1,3 @@
-import { default as OmegaArray } from "./OmegaArray.vue"
-import { default as OmegaAutoGen } from "./OmegaAutoGen.vue"
-import { default as OmegaErrors } from "./OmegaErrors.vue"
-import { default as OmegaFormInput } from "./OmegaFormInput.vue"
 import { default as OmegaInput } from "./OmegaInput.vue"
 import { default as OmegaForm } from "./OmegaWrapper.vue"
 
@@ -10,4 +6,4 @@ export * from "./OmegaFormStuff"
 export { default } from "./OmegaWrapper.vue"
 export { type OmegaFormReturn, useOmegaForm } from "./useOmegaForm"
 
-export { OmegaArray, OmegaAutoGen, OmegaErrors, OmegaForm, OmegaFormInput, OmegaInput }
+export { OmegaForm, OmegaInput }

c5921b190826dfff4f5053445ceee6e6e841d9ec
Patrick Roza <contact@patrickroza.com>
2025-09-21 15:26:30 +0200

    export the OmegaVuetifyInput

--- packages/vue-components/src/components/OmegaForm/index.ts
+++ packages/vue-components/src/components/OmegaForm/index.ts
@@ -4,6 +4,8 @@ export * as OmegaErrorsContext from "./OmegaErrorsContext"
 export * from "./OmegaFormStuff"
 export { type OmegaFormReturn, useOmegaForm } from "./useOmegaForm"
 
+export { default as OmegaVuetifyInput } from "./OmegaInternalInput.vue"
+
 export { getInputType } from "./OmegaFormStuff"
 
 export { OmegaInput }

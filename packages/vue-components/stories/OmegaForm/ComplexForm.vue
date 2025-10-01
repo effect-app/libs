51b3f7dd4a3c67dad02c1e3bf2691de3d35fa6fe
Patrick Roza <contact@patrickroza.com>
2025-10-01 11:21:30 +0200

    enhance complex test with refreshing options, and able to unload a field

--- packages/vue-components/stories/OmegaForm/ComplexForm.vue
+++ packages/vue-components/stories/OmegaForm/ComplexForm.vue
@@ -39,43 +39,47 @@
     <exampleForm.Input
       label="aSelect"
       name="aSelect"
-      :options="[
-        { title: 'a', value: 'a' },
-        { title: 'b', value: 'b' },
-        { title: 'c', value: 'c' }
-      ]"
+      :options="options"
     />
     <exampleForm.Input
       label="aMultiple"
       name="aMultiple"
       type="autocompletemultiple"
-      :options="[
-        { title: 'a', value: 'a' },
-        { title: 'b', value: 'b' }
-      ]"
+      :options="options"
     />
-    <button>Submit</button>
-    <button
+    <exampleForm.Input
+      v-if="autocompleteToggle"
+      label="aSingle"
+      name="aSingle"
+      type="autocomplete"
+      :options="options"
+    />
+    <v-btn type="submit">
+      Submit
+    </v-btn>
+    <v-btn @click="autocompleteToggle = !autocompleteToggle">
+      Toggle aSingle
+    </v-btn>
+    <v-btn
       type="reset"
       @click.prevent="exampleForm.clear()"
     >
       Clear
-    </button>
-    <button
-      type="button"
-      @click="exampleForm.reset()"
-    >
+    </v-btn>
+    <v-btn @click="exampleForm.reset()">
       Reset
-    </button>
+    </v-btn>
     <exampleForm.Errors />
   </exampleForm.Form>
 </template>
 
 <script setup lang="ts">
+import { useIntervalFn } from "@vueuse/core"
 import { S } from "effect-app"
-import { watch } from "vue"
+import { ref, watch } from "vue"
 import { useOmegaForm } from "../../src/components/OmegaForm"
 
+const autocompleteToggle = ref(true)
 const exampleForm = useOmegaForm(
   S.Struct({
     aString: S.String,
@@ -89,6 +93,7 @@ const exampleForm = useOmegaForm(
     aNumberMin2Max: S.Number.pipe(S.greaterThan(2)).pipe(S.lessThan(4)),
     aNumberMin2Max4Nullable: S.NullOr(S.Number.pipe(S.between(2, 4))),
     aSelect: S.Union(S.Literal("a"), S.Literal("b"), S.Literal("c")),
+    aSingle: S.NonEmptyString,
     aMultiple: S.Array(S.String)
   }),
   {
@@ -104,6 +109,7 @@ const exampleForm = useOmegaForm(
         aStringMin2: string
         aStringMin2Max4: string
         aStringMin2Max3Optional?: string
+        aSingle: string
         aNumber: number
         aNumberMin2: number
         aNumberMin2Max: number
@@ -121,6 +127,17 @@ const exampleForm = useOmegaForm(
     }
   }
 )
+
+// simulate options refreshing from api
+const makeOptions = () => [
+  { title: "a", value: "a" },
+  { title: "b", value: "b" },
+  { title: "c", value: "c" }
+]
+const options = ref(makeOptions())
+useIntervalFn(() => {
+  options.value = makeOptions()
+}, 3000)
 const isDirty = exampleForm.useStore((_) => _.isDirty)
 const values = exampleForm.useStore((_) => _.values)
 watch(values, (v) => {

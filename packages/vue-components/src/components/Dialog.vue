a21a722ae2f8dec1446b47e058b2d7949576dcfc
Patrick Roza <contact@patrickroza.com>
2025-10-04 15:48:57 +0200

    feat: add dialog component

---
+++ packages/vue-components/src/components/Dialog.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-dialog
+    v-model="open"
+    persistent
+    @click:outside="onCancel"
+  >
+    <template #default="{ isActive }">
+      <slot
+        :open="open"
+        :cancel="onCancel"
+        :is-active="isActive"
+      />
+    </template>
+    <!-- pass down slots -->
+    <!-- @vue-skip -->
+    <template
+      v-for="(_, name) in $slots"
+      #[name]="slotData"
+    >
+      <slot
+        :name="name"
+        v-bind="slotData"
+      />
+    </template>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+import { useOnClose } from "./OmegaForm/blockDialog"
+import { onMountedWithCleanup } from "./OmegaForm/onMountedWithCleanup"
+
+const props = defineProps<{
+  persistent?: boolean
+}>()
+
+const open = defineModel<boolean>()
+
+const onCancel = useOnClose(() => open.value = false)
+
+onMountedWithCleanup(() => {
+  const onEscape = (e: KeyboardEvent) => {
+    if (open.value && !props.persistent && e.code === "Escape") {
+      onCancel()
+    }
+  }
+
+  document.addEventListener("keydown", onEscape)
+  return () => document.removeEventListener("keydown", onEscape)
+})
+</script>
--- packages/vue-components/src/components/index.ts
+++ packages/vue-components/src/components/index.ts
@@ -1,1 +1,2 @@
+export { default as Dialog } from "./Dialog.vue"
 export * from "./OmegaForm"

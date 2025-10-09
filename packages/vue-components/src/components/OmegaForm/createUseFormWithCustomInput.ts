70b60ebda8b01ef119057775be173509acd0f011
Davide Di Pumpo <davidemak@gmail.com>
2025-10-09 10:55:51 +0200

    Fix/improves wrapped input attributes management 2 (#455)
    * filter attributes in WrappedInput to avoid conflicts with inputProps
    
    * fix: filter attributes in WrappedInput to avoid conflicts with inputProps
    
    * fixes typo and bump version

--- packages/vue-components/src/components/OmegaForm/createUseFormWithCustomInput.ts
+++ packages/vue-components/src/components/OmegaForm/createUseFormWithCustomInput.ts
@@ -31,9 +31,7 @@ export const createUseFormWithCustomInput = <
               // Filter attrs to only pass through attributes that aren't already in inputProps
               // This automatically includes event listeners (on*), data-*, aria-*, etc.
               const filteredAttrs = Object.fromEntries(
-                Object.entries(attrs).filter(([key]) =>
-                  !Object.prototype.hasOwnProperty.call(inputProps.inputProps, key)
-                )
+                Object.entries(attrs).filter(([key]) => !Object.prototype.hasOwnProperty.call(inputProps, key))
               )
               return h(CustomInputComponent, { ...filteredAttrs, field, inputProps })
             }

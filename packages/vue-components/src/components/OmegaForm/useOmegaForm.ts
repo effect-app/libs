910651c9b46ac5f026090fe42f41885aa03d30cb
Davide Di Pumpo <davidemak@gmail.com>
2025-10-24 17:09:44 +0200

    feat: enhance Fieldset type definition for improved prop handling in OmegaForm

--- packages/vue-components/src/components/OmegaForm/useOmegaForm.ts
+++ packages/vue-components/src/components/OmegaForm/useOmegaForm.ts
@@ -230,7 +230,41 @@ export interface OmegaFormReturn<
   ) => import("vue").VNode & {
     __ctx?: Awaited<typeof __VLS_setup>
   }
-  Fieldset: typeof OmegaFieldset // todo type
+  Fieldset: <Name extends DeepKeys<From>>(
+    __VLS_props: NonNullable<Awaited<typeof __VLS_setup>>["props"],
+    __VLS_ctx?: __VLS_PrettifyLocal<Pick<NonNullable<Awaited<typeof __VLS_setup>>, "attrs" | "emit" | "slots">>,
+    __VLS_expose?: NonNullable<Awaited<typeof __VLS_setup>>["expose"],
+    __VLS_setup?: Promise<{
+      props:
+        & __VLS_PrettifyLocal<
+          & Pick<
+            & Partial<{}>
+            & Omit<
+              {} & import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps,
+              never
+            >,
+            never
+          >
+          & {
+            name: Name
+            type?: "select" | "radio"
+            options: import("effect-app").NonEmptyArray<import("./InputProps").FieldsetOption<From, Name>>
+            label?: string
+          }
+          & {}
+        >
+        & import("vue").PublicProps
+      expose(exposed: import("vue").ShallowUnwrapRef<{}>): void
+      attrs: any
+      slots: Record<string, (props: {
+        field: import("@tanstack/vue-form").FieldApi<From, Name, DeepValue<From, Name>, any, any, any, any, any, any, any, any, any, any, any, any, any, any, any, any, any>
+        state: import("@tanstack/vue-form").FieldState<From, Name, DeepValue<From, Name>, any, any, any, any, any, any, any, any, any, any, any, any, any, any, any>
+      }) => any>
+      emit: {}
+    }>
+  ) => import("vue").VNode & {
+    __ctx?: Awaited<typeof __VLS_setup>
+  }
   Array: (
     __VLS_props: NonNullable<Awaited<typeof __VLS_setup>>["props"],
     __VLS_ctx?: __VLS_PrettifyLocal<Pick<NonNullable<Awaited<typeof __VLS_setup>>, "attrs" | "emit" | "slots">>,

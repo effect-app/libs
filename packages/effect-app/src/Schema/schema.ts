2bcfba8fab7f8c0e462dff2fd6f97389985fa699
Patrick Roza <contact@patrickroza.com>
2026-02-24 16:33:26 +0100

    no need here.

--- packages/effect-app/src/Schema/schema.ts
+++ packages/effect-app/src/Schema/schema.ts
@@ -1,10 +1,5 @@
-import type { Schema } from "effect"
 import * as B from "effect/Brand"
 import * as AST from "effect/SchemaAST"
 import * as P from "effect/SchemaParser"
-
-export namespace A {
-  export type LazyArbitrary<T> = Schema.LazyArbitrary<T>
-}
 
 export { AST, B, P }

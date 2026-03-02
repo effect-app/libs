735a5d4c59911c94510afd464e5b8c74f9f8b38b
Patrick Roza <contact@patrickroza.com>
2026-03-02 16:00:03 +0100

    remove effect req from codegen

--- packages/eslint-codegen-model/src/presets/meta.ts
+++ packages/eslint-codegen-model/src/presets/meta.ts
@@ -1,18 +1,17 @@
-import { Array, pipe } from "effect"
 import type { Preset } from "eslint-plugin-codegen"
+
+const filterAdjacent = (input: string[]) => input.filter((i,idx) => input[idx-1] !== i)
 
 /**
  * Adds file meta
  */
 export const meta: Preset<{ sourcePrefix?: string }> = ({ meta, options }) => {
   const sourcePrefix = options.sourcePrefix || "src/"
-  const moduleName = pipe(
-    meta
+  const moduleName =
+    filterAdjacent(meta
       .filename
       .substring(meta.filename.indexOf(sourcePrefix) + sourcePrefix.length, meta.filename.length - 3)
-      .split("/"),
-    Array.dedupeAdjacent
-  )
+      .split("/"))
     .filter((_) => _ !== "resources")
     .join("/")
   const expectedContent = `export const meta = { moduleName: "${moduleName}" } as const`

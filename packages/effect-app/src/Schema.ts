83d824a72829d92afb571824dbe1078f74b1ec88
jfet97 <andrysimo1997@gmail.com>
2026-04-16 12:55:38 +0200

    fix: improve guard handling in extendTaggedUnionWithTags function

--- packages/effect-app/src/Schema.ts
+++ packages/effect-app/src/Schema.ts
@@ -178,7 +178,9 @@ const extendTaggedUnionWithTags = <Members extends TaggedUnionMembers>(
   extendM(schema.pipe(S.toTaggedUnion("_tag")), (tagged) => {
     const makeGuards = (property: string) => {
       const result: any = {}
-      for (const [tag, guard] of Object.entries(tagged.guards)) {
+      const guards: Record<string, (u: unknown) => boolean> = tagged.guards
+      for (const tag of Object.keys(guards)) {
+        const guard = guards[tag]!
         result[`is${tag}`] = (target: any) => guard(target[property])
       }
       result.isAnyOf = (memberTags: Array<string>) => {

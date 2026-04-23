80a34fe91e8ebe820f8814152999c3cb4f7ee7c2
Patrick Roza <contact@patrickroza.com>
2026-04-23 10:03:35 +0200

    switch to tsgo

--- packages/eslint-codegen-model/dist/presets/index.d.ts
+++ packages/eslint-codegen-model/dist/presets/index.d.ts
@@ -1,4 +1,4 @@
 export * from './barrel';
 export * from './model';
 export * from './meta';
-//# sourceMappingURL=index.d.ts.map
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguZC50cyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9wcmVzZXRzL2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUNBLGNBQWMsVUFBVSxDQUFDO0FBQ3pCLGNBQWMsU0FBUyxDQUFDO0FBQ3hCLGNBQWMsUUFBUSxDQUFDIn0=

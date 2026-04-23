49f71c32a4aa195e90840c76ab7c7985824a4c48
Davide Di Pumpo <davidemak@gmail.com>
2026-04-23 16:28:38 +0200

    updates outdated test

--- packages/vue-components/__tests__/OmegaForm/IntersectionError.test.ts
+++ packages/vue-components/__tests__/OmegaForm/IntersectionError.test.ts
@@ -1,6 +1,6 @@
 import { mount } from "@vue/test-utils"
 import { S } from "effect-app"
-import { describe, expect, it } from "vitest"
+import { beforeEach, describe, expect, it } from "vitest"
 import { useOmegaForm } from "../../src/components/OmegaForm"
 import OmegaIntlProvider from "../OmegaIntlProvider.vue"
 
@@ -55,12 +55,13 @@ describe("OmegaForm Intersection/Union", () => {
     myUnion: S.Union([AlphaSchema, BetaSchema])
   })
 
-  const wrapper = mount({
-    components: {
-      OmegaIntlProvider,
-      CustomInput: mockComponents.CustomInput
-    },
-    template: `
+  const createWrapper = () =>
+    mount({
+      components: {
+        OmegaIntlProvider,
+        CustomInput: mockComponents.CustomInput
+      },
+      template: `
       <OmegaIntlProvider>
         <component :is="form.Form" :subscribe="['values']">
           <template #default="{ subscribedValues: { values } }">
@@ -104,22 +105,28 @@ describe("OmegaForm Intersection/Union", () => {
         </component>
       </OmegaIntlProvider>
     `,
-    setup() {
-      let submittedValue: any = null
-      const form = useOmegaForm(MySchema, {
-        onSubmit: async ({ value }) => {
-          submittedValue = value
-        }
-      })
-      return { form, getSubmittedValue: () => submittedValue }
-    }
-  }, {
-    global: {
-      stubs: {
-        CustomInput: mockComponents.CustomInput,
-        OmegaErrorsInternal: mockComponents.OmegaErrors
+      setup() {
+        let submittedValue: any = null
+        const form = useOmegaForm(MySchema, {
+          onSubmit: async ({ value }) => {
+            submittedValue = value
+          }
+        })
+        return { form, getSubmittedValue: () => submittedValue }
       }
-    }
+    }, {
+      global: {
+        stubs: {
+          CustomInput: mockComponents.CustomInput,
+          OmegaErrorsInternal: mockComponents.OmegaErrors
+        }
+      }
+    })
+
+  let wrapper: ReturnType<typeof createWrapper>
+
+  beforeEach(() => {
+    wrapper = createWrapper()
   })
 
   it("handles discriminated union with conditional fields", async () => {

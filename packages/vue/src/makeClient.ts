4afd01d70f15d303552010e063ff9b79ff62d5eb
Patrick Roza <contact@patrickroza.com>
2026-04-25 15:40:02 +0200

    style format make-based command input type

--- packages/vue/src/makeClient.ts
+++ packages/vue/src/makeClient.ts
@@ -663,8 +663,8 @@ export interface CommandBase<I = void, A = void> {
 
 export interface EffectCommand<I = void, A = unknown, E = unknown> extends CommandBase<I, Fiber<A, E>> {}
 
-type RequestInputFromMake<I extends { readonly make: (...args: any[]) => any }> =
-  Parameters<I["make"]> extends [infer A, ...ReadonlyArray<any>] ? A : void
+type RequestInputFromMake<I extends { readonly make: (...args: any[]) => any }> = Parameters<I["make"]> extends
+  [infer A, ...ReadonlyArray<any>] ? A : void
 
 export interface CommandFromRequest<I extends { readonly make: (...args: any[]) => any }, A = unknown, E = unknown>
   extends EffectCommand<RequestInputFromMake<I>, A, E>

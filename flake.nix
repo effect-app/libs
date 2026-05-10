fbebd61dd21af0e4ae73d43ad15f6c3b8bfbffae
Patrick Roza <contact@patrickroza.com>
2026-05-10 19:08:21 +0200

    pnpm 11 bs

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Node 24 + pnpm 10 dev shell";
+  description = "Node 24 + pnpm 11 dev shell";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -11,7 +11,13 @@
       let
         pkgs = import nixpkgs { inherit system; };
         nodejs = pkgs.nodejs_24;
-        pnpm = pkgs.pnpm.override { inherit nodejs; };
+        pnpm = (pkgs.pnpm.override { inherit nodejs; }).overrideAttrs (old: rec {
+          version = "11.0.9";
+          src = pkgs.fetchurl {
+            url = "https://registry.npmjs.org/pnpm/-/pnpm-${version}.tgz";
+            hash = "sha256-TYTXsOMckFT2Flh5VpgHAAfQO3I4SB4hYaViVXqpCDQ=";
+          };
+        });
         tools = with pkgs; [
           git
           nixfmt-classic

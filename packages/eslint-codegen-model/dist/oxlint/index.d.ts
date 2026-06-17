20d0f8bf27941242e16ab4d5a998359645889a0b
Patrick Roza <contact@patrickroza.com>
2026-06-17 09:17:03 +0200

    update tsgo

--- packages/eslint-codegen-model/dist/oxlint/index.d.ts
+++ packages/eslint-codegen-model/dist/oxlint/index.d.ts
@@ -7,4 +7,4 @@ declare const _default: {
     };
 };
 export default _default;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguZC50cyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9veGxpbnQvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtJQUdFLElBQUk7UUFBSSxJQUFJOztJQUNaLEtBQUs7UUFDSCxPQUFPIn0=
+//# sourceMappingURL=index.d.ts.map
--- packages/eslint-codegen-model/dist/presets/index.d.ts
+++ packages/eslint-codegen-model/dist/presets/index.d.ts
@@ -2,4 +2,4 @@ export * from "./barrel.js";
 export * from "./meta.js";
 export * from "./model-facade.js";
 export * from "./model.js";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguZC50cyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9wcmVzZXRzL2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUNBLGNBQWMsYUFBYSxDQUFBO0FBQzNCLGNBQWMsV0FBVyxDQUFBO0FBQ3pCLGNBQWMsbUJBQW1CLENBQUE7QUFDakMsY0FBYyxZQUFZLENBQUEifQ==
+//# sourceMappingURL=index.d.ts.map

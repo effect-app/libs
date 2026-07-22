275b6b0c3157659d3992e3be347deb4d287be813
MindfulLearner <dimajosh1009@gmail.com>
2026-07-22 18:09:51 +0200

    refactor(): added tests, and fixednuxterrorboundary in libs,added storybook demo

--- packages/vue-components/src/components/index.ts
+++ packages/vue-components/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as CommandButton } from "./CommandButton.vue"
 export { default as Dialog } from "./Dialog.vue"
+export { default as FixedNuxtErrorBoundary } from "./FixedNuxtErrorBoundary.vue"
 export * from "./OmegaForm"
